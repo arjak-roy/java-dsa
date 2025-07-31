ca71b89eb07ed51475731bb550ccfb69f2de6f7f
arjakroy <arjakroy2411@gmail.com>
2025-08-01 01:11:19 +0530

    first commit

---
+++ Arrays/2Sum/sol.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class sol {
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement)) {
+                return new int[]{numMap.get(complement), i};
+            }
+            numMap.put(nums[i], i);
+        }
+
+        return new int[]{}; // No solution found
+    }
+
+    public static void main(String args[]){
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = twoSum(nums, target);
+        System.out.println(Arrays.toString(result));
+    }
+
+}
